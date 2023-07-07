354cc768b911f91631e0d7286cb170479e8b02f2
Samuel Paulo <112398583+MuelTec@users.noreply.github.com>
2023-07-07 14:21:37 -0300

    carrinho com numeração

--- src/components/Carrinho/Carrinho.jsx
+++ src/components/Carrinho/Carrinho.jsx
@@ -3,20 +3,20 @@ import React, { useContext } from 'react';
 import './carrinho.css';
 import CartItem from '../Cartitem/CartItem';
 import AppContext from '../../context/AppContext';
+import fomartCurrency from '../../utils/formatCurrency';
 
 const Carrinho = () => {
   const { cartItems } = useContext(AppContext);
 
+  const totalPrice = cartItems.reduce((acc, item) => item.price + acc, 0);
+
   return (
-    <section className="carrinho">
+    <section className="carrinho ">
       <div className="carrinhos-items">
         {cartItems.map((cartItem) => <CartItem key={cartItem.id} data={cartItem} />)}
-
-
-        
       </div>
 
-      <div className="carrinho-resumo">resumo do carrinho</div>
+      <div className="carrinho-resumo">{fomartCurrency(totalPrice, 'BRL')}</div>
     </section>
   );
 };
--- src/components/CartButton/CartButton.jsx
+++ src/components/CartButton/CartButton.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {AiOutlineShoppingCart} from 'react-icons/ai';
 import './CartButton.css';
+import AppContext from '../../context/AppContext';
 
 const CartButton = () => {
+
+  const { cartItems } = useContext(AppContext);
+
   return (
     <button type="button" className="cart-button">
       <AiOutlineShoppingCart />
-      <span className="cart-status">1</span>
+      <span className="cart-status">{cartItems.length}</span>
     </button>
   );
 };
